93e04c298bb662f06d2bf33984e39b976a6505ae
24HP9677 24HP9677 <24HP9677@G303-CC0136.local>
2024-07-25 15:25:12 +0800

    vgrfedsx

--- src/graphql/resolver/mutiation/login.ts
+++ src/graphql/resolver/mutiation/login.ts
@@ -7,8 +7,6 @@ export const login = async (_: any, input: MutationLoginArgs) => {
     const user = await userModel.findOne({ email: email, password: password });
     if (!user) {
       throw new Error("User not found");
-    } else if (password === user.password) {
-      throw new Error("Password is wrong");
     }
     return user;
   } catch (err) {
